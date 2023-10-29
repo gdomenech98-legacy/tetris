1df47ff4fc7cc1fdf42c20832f3cd5e19b04e1a6
Gerard <gerard@protofy.xyz>
2023-10-29 15:51:42 +0100

    fix board size

--- src/models/Board.py
+++ src/models/Board.py
@@ -3,22 +3,18 @@ import pygame
 from ..utils.Shapes import getCellColor
 
 # Board
-BOARD_WIDTH = 10  # Number of columns
-BOARD_HEIGHT = 20  # Number of rows
 CELL_SIZE = 30  # Size of each cell in pixels
-BOARD_POS_X = 30  # X-coordinate of the top-left corner of the board
-BOARD_POS_Y = 30  # Y-coordinate of the top-left corner of the board
 # Screen
 SCREEN_WIDTH = 300
 SCREEN_HEIGHT = 600
 class Board:
     def __init__(self):
-        self.board_width = BOARD_WIDTH
-        self.board_height = BOARD_HEIGHT
+        self.board_width = int(SCREEN_WIDTH/CELL_SIZE)
+        self.board_height = int(SCREEN_HEIGHT/CELL_SIZE)
         self.board = self.emptyBoard() # Initialize the game board grid
 
     def emptyBoard(self):
-        return [[0] * BOARD_WIDTH for _ in range(BOARD_HEIGHT)]
+        return [[0] * self.board_width for _ in range(self.board_height)]
     
     def getWidth(self):
         return self.board_width
@@ -40,8 +36,8 @@ class Board:
                 if(cell):
                     cell_color = getCellColor(cell)
                     cell_rect = pygame.Rect(
-                        BOARD_POS_X + ci * CELL_SIZE,
-                        BOARD_POS_Y + ri * CELL_SIZE,
+                        ci * CELL_SIZE,
+                        ri * CELL_SIZE,
                         CELL_SIZE,
                         CELL_SIZE
                     )
